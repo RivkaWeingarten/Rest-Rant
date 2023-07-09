08f3d7a1e74d7843d4556e218eaf5610b4cb0a54
Rivka Weingarten <126045006+RivkaWeingarten@users.noreply.github.com>
2023-07-09 16:32:03 -0400

    homework done

--- controllers/places.js
+++ controllers/places.js
@@ -1,42 +1,42 @@
 // const router = require("express").Router();
-const express = require('express')
-const app = express()
-require('dotenv').config()
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const places = require("../models/places.js");
 
 // const app = express()
 // router.get("/", (req, res) => {
 //   res.send("GET /places");
 // });
 
-app.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
-})
-
-
-app.get('/new', (req, res) => {
-  res.render('places/new')
-})
-
 app.get("/", (req, res) => {
-
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/restaurant.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/coffee.jpg'
-  }]
-
-  
-  res.render('places/index', {places});
+  res.render("places/index", { places });
 });
 
+// More code ...
+
+app.post("/", (req, res) => {
+  // console.log(req.body);
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
+});
+
+app.get("/new", (req, res) => {
+  res.render("places/new");
+});
+
+app.get("/", (req, res) => {
+  res.render("places/index", { places });
+});
 
 module.exports = app;
