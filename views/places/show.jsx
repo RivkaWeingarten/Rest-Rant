053abe0be3f39a201d0ab417c033a4e2dcad43d1
Rivka Weingarten <126045006+RivkaWeingarten@users.noreply.github.com>
2023-07-16 15:54:02 -0400

    added add element and founded year with validations

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,30 +5,39 @@ function show(data) {
   return (
     <Def>
       <main>
-        <div>
-          <h1>{data.place.name}</h1>
-          <h2>Rating</h2>
-          <p>Not Rated</p>
-          <h2>Description</h2>
-          <p>
-            Located in {data.place.city}, {data.place.state} and serving{" "}
-            {data.place.cuisines}
-          </p>
-          <p className="text-center">{data.place.cuisines}</p>
-          <img src={data.place.pic} alt={data.place.name} />
-          <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-            Edit
-          </a>
-          <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-            <button type="submit" className="btn btn-danger">
-              Delete
-            </button>
-          </form>
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+            </div>
+            <div className="col-sm-6">
+            <h1>{data.place.name}</h1>
+            <h2>Rating</h2>
+            <p>Not Rated</p>
+            <h2>Description</h2>
+            <h3>
+          {data.place.showEstablished()}
+        </h3>
+        <h4>
+          Serving {data.place.cuisines}
+        </h4>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+              Edit
+            </a>
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+              <button type="submit" className="btn btn-danger">
+                Delete
+              </button>
+            </form>
+          </div>
+         
         </div>
-        <div>
+        <div className="align-items-center">
           <h2>Comments</h2>
           <p>No Comments yet </p>
-        </div>
+          </div>
       </main>
     </Def>
   );
