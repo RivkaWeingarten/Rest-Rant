5e0653f11c14ffd2bf4c63bac310cb3252293da2
Rivka Weingarten <126045006+RivkaWeingarten@users.noreply.github.com>
2023-07-17 23:07:03 -0400

    Found error! thanks Jeremy again!

--- controllers/places.js
+++ controllers/places.js
@@ -16,16 +16,16 @@ router.get('/', (req, res) => {
 })
 
 
-router.post('/', (req, res) => {
-  db.Place.create(req.body)
-  .then(() => {
-      res.redirect('/places')
-  })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
-})
+// router.post('/', (req, res) => {
+//   db.Place.create(req.body)
+//   .then(() => {
+//       res.redirect('/places')
+//   })
+//   .catch(err => {
+//       console.log('err', err)
+//       res.render('error404')
+//   })
+// })
 
 
 router.get('/new', (req, res) => {
